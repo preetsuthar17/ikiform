ca4f91064399fb993b9129b1add7e7271c42c119
preetsuthar17 <preetsutharxd@gmail.com>
2025-07-03 08:09:53 +0530

    chore: Remove outdated database README and update logo assets
    - Deleted the obsolete README.md file for database setup.
    - Updated favicon and logo images, including new dark and light versions.
    - Modified the logo SVG for improved design consistency.
    - Added a new text logo SVG for branding purposes.

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -2,19 +2,32 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export default function Header() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <nav className="flex justify-between flex-wrap items-center gap-8  py-10 text-sm font-inter max-sm:flex-col max-sm:text-center max-sm:items-center max-sm:justify-center max-w-[95%] mx-auto w-full px-6">
       <div className="max-w-[90px]">
         <Link href="/">
-          <Image
-            src="/logo.svg"
-            alt="ikiform"
-            width={40}
-            height={40}
-            className="pointer-events-none rounded-ele"
-          />
+          {theme === "dark" ? (
+            <Image
+              src="/text-logo.svg"
+              alt="Ikiform"
+              width={100}
+              height={100}
+              className="pointer-events-none invert"
+            />
+          ) : (
+            <Image
+              src="/text-logo.svg"
+              alt="Ikiform"
+              width={100}
+              height={100}
+              className="pointer-events-none"
+            />
+          )}
         </Link>
       </div>
       <div className="flex items-center gap-4">
