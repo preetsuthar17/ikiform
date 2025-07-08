c43948efd1398de0d4c7a1f5140bb2f2d5970ef4
preetsuthar17 <preetsutharxd@gmail.com>
2025-07-08 14:54:16 +0530

    refactor: Clean up home page layout by removing the separator component
    - Removed the Separator component from the home page for a more streamlined design.
    - Maintained the overall structure while enhancing visual clarity and focus on key features.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,11 @@
 import Hero from "@/components/home/hero";
 import Features from "@/components/home/features";
 
-import { Separator } from "@/components/ui/separator";
-
 export default function Home() {
   return (
     <>
       <main>
         <Hero />
-        <Separator />
         <Features />
       </main>
     </>
