1b5d2b6cf63ec283de471417041a3c54ff2beadf
preetsuthar17 <preetsutharxd@gmail.com>
2025-07-09 19:50:23 +0530

    fix: Adjust Review component layout and styling
    - Increased gap between elements in the Review component for better spacing.
    - Reduced image dimensions for a more compact design.
    - Updated role text size for improved readability.

--- src/components/other/reivew.tsx
+++ src/components/other/reivew.tsx
@@ -23,19 +23,19 @@ export default function Review({
           <Star size={20} fill="#eab308" stroke="none" />
         </div>
         <p className="text-center">{review}</p>
-        <div className="flex items-center justify-center gap-2">
+        <div className="flex items-center justify-center gap-3">
           <div className="rounded-full overflow-ellipsis">
             <OptimizedImage
               src={image}
-              width={50}
-              height={50}
+              width={45}
+              height={45}
               alt={name}
               className="rounded-full"
             />
           </div>
           <div className="flex items-start justify-center flex-col">
             <p>{name}</p>
-            <p className="text-muted-foreground">{role}</p>
+            <p className="text-muted-foreground text-sm">{role}</p>
           </div>
         </div>
       </div>
