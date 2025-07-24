b020d20f2db2fb4f146cc30dbfc917c7a7f87a6d
preetsuthar17 <preetsutharxd@gmail.com>
2025-07-24 09:35:02 +0530

    refactor: update shadow color in CTASection for consistency with design system

--- src/components/home/cta-section.tsx
+++ src/components/home/cta-section.tsx
@@ -7,7 +7,7 @@ export default function CTASection() {
   return (
     <section className="flex flex-col items-center justify-center gap-12 md:py-28 py-12 md:px-8 px-4 text-center w-full relative">
       <div className="max-w-7xl w-full mx-auto flex flex-col gap-18 relative">
-        <div className="flex flex-col items-center justify-center gap-3 px-6 py-12 md:py-16 backdrop-blur-[0px] w-full rounded-card z-4 shadow-[inset_0px_-24px_66px_-11px_rgba(0,_0,_0,_0.1)]">
+        <div className="flex flex-col items-center justify-center gap-3 px-6 py-12 md:py-16 backdrop-blur-[0px] w-full rounded-card z-4 shadow-[inset_0px_-24px_66px_-11px_hsl(var(--hu-primary),0.1)]">
           <p className="text-5xl max-[414px]:text-4xl md:text-7xl tracking-tight leading-tight font-medium">
             Ready to create beautiful forms?
           </p>
