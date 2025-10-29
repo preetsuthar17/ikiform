32535e8a3bffc1f40ccc4e085c1b152f9a9cd579
preetsuthar17 <preetsutharxd@gmail.com>
2025-10-29 14:05:37 +0530

    refactor: adjust layout behavior to conditionally render Footer based on current pathname

--- src/app/conditional-layout.tsx
+++ src/app/conditional-layout.tsx
@@ -23,16 +23,21 @@ export default function ConditionalLayout({
     pathname.includes("/f") ||
     pathname.includes("/demo-form-builder");
 
+  // If we are on /dashboard, do not render the Footer
+  const isDashboard = pathname === "/dashboard";
+
   if (hideHeaderFooter) {
     return <>{children}</>;
   }
 
   return (
-    <div className="z-10 flex min-h-screen flex-col justify-between gap-12 px-4 md:px-8">
+    <div
+      className={`z-10 flex min-h-screen flex-col gap-12 px-4 md:px-8 ${isDashboard ? "justify-start" : "justify-between"} ${isDashboard ? "pb-12" : ""}`}
+    >
       <Header />
       <TrialBannerWrapper />
       {children}
-      <Footer />
+      {!isDashboard && <Footer />}
     </div>
   );
 }
